dd6b3bdd5070da2b332fbf235a91dd14844ee117
@fernando_rp <alex_3099@hotmail.com>
2022-12-01 16:01:33 -0600

    Cambios 2.2

--- controlador/usuarioControl.js
+++ controlador/usuarioControl.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const bcrypt = require('bcrypt');
-const app = require('../app');
-
 var usuariosModelo = require('../modelo/usuarios');
+var usuarios = new usuariosModelo();
+var jwt = require('../servicio/jwt');
+var fs = require('fs');
+var path = require('path');
 
 
 
@@ -18,94 +20,76 @@ module.exports= {
 };
 
 function registrarUsuario(req, res){
-    var usuario = new usuariosModelo();
-
-    var params = req.body; //Recibe los datoss por post
+    var params = req.body; //recibe todos los datos por Por el Metodo POST
     console.log(params);
-    usuario.id = params.id
-    usuario.nombre = params.nombre;
-    usuario.apellido = params.apellido;
-    usuario.email = params.email;
-    usuario.password = params.password;
-    usuario.rol = 'ROLE_USER';
-    usuario.imagen = 'null';
+    usuarios.nombre = params.nombre;
+    usuarios.apellido = params.apellido;
+    usuarios.email = params.email;
+    usuarios.rol = 'ROLE_ADMIN';
+    usuarios.imagen = 'null';
 
-    if(params.password){
-
-        bcrypt.hash(params.password, 10, function(err, hash){
-            usuario.password = hash;
-
-            if(usuario.nombre != null && usuario.apellido != null && usuario.email != null){
-                //guardar el usuario en BD
-                usuario.save((err, usuarioAlmacenado) => {
-                    if (err){
-                        res.status(500).send({mesagge: 'Error al guardar el usuario'});
+    if (params.password) {
+        bcrypt.hash(params.password, 10, function(err, hash) {
+            usuarios.password = hash;
+            if (usuarios.nombre != null && usuarios.apellido != null && usuarios.email != null) {
+                //guardar el ususario en BD
+                usuarios.save((err, usuarioAlmacenado) => {
+                    if (err) {
+                        res.status(500).send({ mesagge: 'Error al guardar el usuario' });
                     } else {
-                        if (!usuarioAlmacenado){
-                            res.status(404).send({mesagge: 'No se ha registrado el usuario'});
-                        } else{
-                        // Nos devuelve un objero con los datos del usuario guardado
-                        res.status(200).send({usuarios: usuarioAlmacenado});
+                        if (!usuarioAlmacenado) {
+                            res.status(404).send({ mesagge: 'No se ha registrado el usuario' });
+                        } else {
+                            //nos devuelve un objeto con los datos del ususario guardado
+                            res.status(200).send({ usuarios: usuarioAlmacenado });
+                            console.log(usuarioAlmacenado);
                         }
                     }
                 });
             } else {
-                res.status(200).send({mesagge: 'Introduce todos los campos'});
+                res.status(200).send({ mesagge: 'Introduce todos los campos' });
             }
         });
-        //encriptar contraseña y guardar datos
-    } else{
-        res.status(500).send({mesagge: 'introduce la contraseña'})
+
+    } else {
+        res.status(404).send({ mesagge: 'Introduce la contraseña' });
     }
+
 }
 
-function accesoUsuario(req, res){
+function accesoUsuario(req, res) {
     var params = req.body;
     var email = params.email;
     var password = params.password;
 
-    usuariosModelo.findOne({ email: email}, (err, user) =>{
-        if (err){
-            res.status(500).send({mesagge: 'Error en la peticion'});
-        } else{
-        } if (!user){
-            res.status(404).send({mesagge: 'El usuario no existe'});
-        } else{
-            bcrypt.compare (password, usuario.password, function (err, check){
-                if (check){
-                    //Devocer los datos del usuario Logeado
-                    console.log('Coincide el password')
-                    if (params.gethash){
-                        //Devolver un token de jwt
-                    } else {
-                        res.status(200).send({ user: user});
-                    }
-                } else{
-                    res.status(404).send({mesagge: 'El usuario no se ha identificado'});
-                }
-            });
-        }
-    });
-}
-
-function actualizarUsuario(req, res) { //PUT
-    var userId = req.params.id; //GET
-    var update = req.body //POST
-
-    usuariosModelo.findByIdAndUpdate(userId, update, (err, userUpdate) => {
+    usuariosModelo.findOne({ email: email }, (err, user) => {
         if (err) {
-            res.status(500).send({ message: 'Error al actualizar el usuario en el servidor' });
+            res.status(500).send({ mesagge: 'Error en la peticion al servidor' });
         } else {
-            if (!userUpdate) {
-                res.status(404).send({ message: 'No se ha podido encontar el usuario' });
+            if (!user) {
+                res.status(404).send({ mesagge: 'El usuario no existe' });
             } else {
-                res.status(200).send({ user: userUpdate });
+                bcrypt.compare(password, usuarios.password, function(err, check) {
+                    if (check) {
+                        //devolver los datos del ususario logeado
+                        console.log('coincide el password')
+                        if (params.gethash) {
+                            res.status(200).send({
+                                token: jwt.createToken(user)
+                            });
+                            //devolver un token de jwt
+                        } else {
+                            res.status(200).send({ user: user });
+                        }
+                    } else {
+                        res.status(404).send({ mesagge: 'El usuario no se ha identificado' });
+                    }
+                });
             }
         }
     });
 }
 
-
 module.exports={
-    prueba, registrarUsuario, accesoUsuario, actualizarUsuario
+    prueba, registrarUsuario, accesoUsuario
 };
